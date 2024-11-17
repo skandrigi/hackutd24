72190e3fb9ae68341e742dfd8d67d69d9a3f88fd
nafi-islam <nafi.islam321@gmail.com>
2024-11-17 01:27:14 -0600

    esp32 websocket connection test

--- embedded/mic_test.cpp
+++ embedded/mic_test.cpp
@@ -1,7 +1,9 @@
 const int micPin = 36; // ADC1_0 (GPIO36)
+
 void setup() {
     Serial.begin(115200); // Serial communication baud rate
 }
+
 void loop() {
     int micValue = analogRead(micPin); 
     Serial.println(micValue);    
---
+++ embedded/websocket_audio.cpp
@@ -0,0 +1,68 @@
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+
+using namespace websockets;
+
+// Wi-Fi Info
+const char* ssid = ""; // Manually set when we run
+const char* password = ""; // Manually set when we run
+
+// WebSocket server URL
+const char* websocket_server = ""; // Manually set when we run
+
+WebsocketsClient webSocket;
+
+const int micPin = 36; // ADC pin for the solo mic
+const int samplingRate = 8000; // Target sampling rate (8kHz), might need to adjust
+
+void connectToWiFi() {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to Wi-Fi...");
+    }
+    Serial.println("Connected to Wi-Fi");
+}
+
+void connectToWebSocket() {
+    if (webSocket.connect(websocket_server)) {
+        Serial.println("Connected to WebSocket server");
+    } else {
+        Serial.println("WebSocket connection failed");
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    // Connect to Wi-Fi
+    connectToWiFi();
+
+    // Connect to WebSocket server
+    connectToWebSocket();
+}
+
+void loop() {
+    static unsigned long lastSampleTime = 0;
+    unsigned long currentTime = micros();
+
+    // Maintain a consistent sampling rate
+    if (currentTime - lastSampleTime >= (1000000 / samplingRate)) {
+        lastSampleTime = currentTime;
+
+        // Read ADC value from microphone
+        int micValue = analogRead(micPin);
+
+        // Send a message to WebSocket server
+        webSocket.send("Hello WebSocket!");
+
+        // Send ADC value as raw audio data to WebSocket server
+        webSocket.send(String(micValue));
+
+        // Print value for debugging
+        Serial.println(micValue);
+    }
+
+    // Keep WebSocket connection alive
+    webSocket.poll();
+}
